0696c9a0f0035633e407b8a23dd4ae01e585473d
fyang151 <fyang151@gmail.com>
2025-03-01 17:30:51 -0800

    adslkfjdlaksfkjdlsfj

--- app/api/beads/route.js
+++ app/api/beads/route.js
@@ -2,16 +2,16 @@ import { createClient } from "../../../utils/supabase/server";
 import { NextResponse } from "next/server";
 
 // fetch all bead data
-export async function GET(req) {
+export async function GET() {
   try {
-    const supabase = await createClient();
+    const supabase = createClient(); // dsjfladsjfk ldjsfl
     const { data } = await supabase.from("beads").select();
 
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
-};
+}
 
 // request to add a new bead
 export async function POST(req) {
@@ -19,14 +19,9 @@ export async function POST(req) {
     // make body of post request
     const body = await req.json();
 
-    const supabase = await createClient();
-    const res = await supabase.from("beads").insert({ image_path: body.image_path,
-                                                      diameter_mm: body.diameter_mm,
-                                                      price: body.price,
-                                                      colour: body.colour,
-                                                      shape: body.shape,
-                                                      stock: body.stock
-    })
+    const supabase = createClient(); // dsjfladsjfk ldjsfl
+    // could we just do this?
+    const res = await supabase.from("beads").insert(body);
     return NextResponse.json(res);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 400 });
