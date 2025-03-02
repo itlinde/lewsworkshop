ce8a72c14d15b31a521eb495e13cf94fd4fcd6c3
fyang151 <fyang151@gmail.com>
2025-03-01 17:27:13 -0800

    orders route quetsions

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -3,15 +3,16 @@ import { NextResponse } from "next/server";
 
 // fetch all order data
 export async function GET(req) {
+  // this req param is unused do we need it in the arguments?
   try {
-    const supabase = await createClient();
+    const supabase = createClient(); // do we need to await this? i just removed that and we got no errors
     const { data } = await supabase.from("orders").select();
 
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
-};
+}
 
 // request to add a new order
 export async function POST(req) {
@@ -19,14 +20,9 @@ export async function POST(req) {
     // make body of post request
     const body = await req.json();
 
-    const supabase = await createClient();
-    const res = await supabase.from("orders").insert({ 
-                                                      total: body.total,
-                                                      status: body.status,
-                                                      delivery_method: body.delivery_method,
-                                                      customer_id: body.customer_id,
-                                                      date_ordered: body.date_ordered
-    })
+    const supabase = createClient(); // this too
+    // could we just do this?
+    const res = await supabase.from("orders").insert(body);
     return NextResponse.json(res);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 400 });
