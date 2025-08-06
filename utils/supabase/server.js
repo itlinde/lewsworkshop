54196c5522de7675f3b256b029d33f8b73e1256d
fyang151 <fyang151@gmail.com>
2025-08-05 20:57:20 -0700

    update role key

--- utils/supabase/server.js
+++ utils/supabase/server.js
@@ -1,28 +1,8 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server"; 
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
-export async function createClient() {
-  const cookieStore = await cookies();
-
-  return createServerClient(
+export function createClient() {
+  return createSupabaseClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch (error) {
-            return NextResponse.json({ error: error.message }, { status: 400 });
-          }
-        },
-      },
-    }
+    process.env.SUPABASE_SERVICE_ROLE_KEY
   );
 }
