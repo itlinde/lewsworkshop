53c8ac77df09b9ac2f266a09b8354751f622693d
Isabella Linde <isabellalinde1770@gmail.com>
2025-09-09 18:53:09 -0700

    fix bead box items not centering on safari

--- app/components/BeadBox.js
+++ app/components/BeadBox.js
@@ -5,7 +5,7 @@ const BeadBox = ({ onClick, imagePath, price, diameter }) => {
   const scaledSize = diameter * 6;
 
   return (
-    <button onClick={onClick} className="group relative block size-[118px] md:size-36 border-backgroundDark border-[1.5px] place-items-center place-content-center rounded-2xl text-center">
+    <button onClick={onClick} className="group relative grid size-[118px] md:size-36 border-backgroundDark border-[1.5px] place-content-center rounded-2xl text-center">
         <Image
           className="object-contain group-hover:scale-110 transition ease-in-out duration-200"
           src={imagePath}
@@ -21,7 +21,7 @@ const BeadBox = ({ onClick, imagePath, price, diameter }) => {
           {/* <p className="text-4xl text-textDark place-self-center">+</p> */}
           <Image className="text-textDark scale-50 group-hover:scale-110 transition ease-in-out" src={PlusIcon} alt=""/>  
         </div>
-        <p className="text-xs text-textLight absolute bottom-0 py-1.5 px-3 opacity-100 group-hover:opacity-100">${(price / 100).toFixed(2)}</p>
+        <p className="text-xs text-textLight absolute place-self-center bottom-0 py-1.5 px-3 opacity-100 group-hover:opacity-100">${(price / 100).toFixed(2)}</p>
     </button>
   );
 };
